718731e7110d5a1b9d44e421151294c027e25cd9
Jelani Woods <jelani@firstdraft.com>
2020-08-14 15:56:47 -0500

    Update user_auth_spec.rb

--- spec/features/user_auth_spec.rb
+++ spec/features/user_auth_spec.rb
@@ -1,23 +1,25 @@
 require "rails_helper"
 
-describe "/photos - Create photo form" do
-  it "automatically populates owner_id of new photo with id of the signed in user", points: 2 do
+describe "/users/[USERNAME] - Update user form" do
+  it "does not display Update user form when logged in user is on another user's page", points: 2 do
     first_user = User.new
-    first_user.email = "first@example.com"
-    if User.has_attribute?(:username)
-      first_user.username = "alice"
-    end
     first_user.password = "password"
+    first_user.username = "alice"
+    if User.has_attribute?(:email)
+      first_user.email = "alice@example.com"
+    end
     first_user.save
 
-    photo = Photo.new
-    photo.image = "https://some.test/image-#{Time.now.to_i}.jpg"
-    photo.caption = "Some test caption #{Time.now.to_i}"
-    photo.owner_id = first_user.id
-    photo.save
+    bob = User.new
+    bob.password = "password"
+    bob.username = "bob"
+    if User.has_attribute?(:email)
+      bob.email = "bob@example.com"
+    end
+    bob.save
 
     visit "/user_sign_in"
-    
+        
     type = "email"
     name_label = all("label", :text => /Email/i)
     if name_label.count.positive?
@@ -49,30 +51,225 @@ describe "/photos - Create photo form" do
     within(:css, "form") do
       # fill_in "Email", with: first_user.email
       fill_in "Password", with: first_user.password
-      click_on "Sign in"
+      find("button", :text => /Sign in/i ).click
     end
-    
-    visit "/photos"
-    current_time = Time.now.to_i
-    within(:css, "form") do
-      fill_in "Image", with: "https://some.test/image-#{current_time}.jpg"
-      fill_in "Caption", with: "Some test caption #{current_time}"
-      click_on "Add photo"
-    end
-    
 
-    expect(page).to have_text("Some test caption #{current_time}")
+    
+    visit "/users/#{bob.username}"
+
+    expect(page).not_to have_tag("form"),
+      "Expected page to not have a form to edit the user but found one anyway."
+  end
+end
+
+describe "/users/[USERNAME] - Update user form" do
+  it "does display Update user form when logged in user is on their own page", points: 2 do
+    first_user = User.new
+    first_user.password = "password"
+    first_user.username = "alice"
+    if User.has_attribute?(:email)
+      first_user.email = "alice@example.com"
+    end
+    first_user.save
+
+    bob = User.new
+    bob.password = "password"
+    bob.username = "bob"
+    if User.has_attribute?(:email)
+      bob.email = "bob@example.com"
+    end
+    bob.save
+
+    visit "/user_sign_in"
+        
+    type = "email"
+    name_label = all("label", :text => /Email/i)
+    if name_label.count.positive?
+      name_label = name_label.first
+    else
+      name_label = find("label", :text => /Username/i)
+      type = "username"
+    end
+    
+    for_attribute = name_label[:for]
+    
+    if for_attribute.nil?
+      expect(for_attribute).to_not be_empty,
+        "Expected label's for attribute to be set to a non empty value, was '#{for_attribute}' instead."
+    else
+      all_inputs = all("input")
+  
+      all_inputs.each do |input|
+        if input[:id] == for_attribute
+          if type == "email"
+            input.set first_user.email
+          else
+            input.set first_user.username
+          end
+        end
+      end
+    end
+
+    within(:css, "form") do
+      # fill_in "Email", with: first_user.email
+      fill_in "Password", with: first_user.password
+      find("button", :text => /Sign in/i ).click
+    end
+
+    
+    visit "/users/#{first_user.username}"
+
+    expect(page).to have_tag("form"),
+      "Expected page to have a form to edit the user but didn't find one."
+  end
+end
+
+
+describe "/photos - Create photo form" do
+  it "automatically populates owner_id of new photo with id of the signed in user", points: 2 do
+    first_user = User.new
+    first_user.password = "password"
+    first_user.username = "alice"
+    if User.has_attribute?(:email)
+      first_user.email = "alice@example.com"
+    end
+    first_user.save
+
+    new_caption = "Some new test caption #{Time.now.to_i}"
+
+    photo = Photo.new
+    photo.image = "https://some.test/image-#{Time.now.to_i}.jpg"
+    photo.caption = new_caption
+    photo.owner_id = first_user.id
+    photo.save
+
+    visit "/user_sign_in"
+        
+    type = "email"
+    name_label = all("label", :text => /Email/i)
+    if name_label.count.positive?
+      name_label = name_label.first
+    else
+      name_label = find("label", :text => /Username/i)
+      type = "username"
+    end
+    
+    for_attribute = name_label[:for]
+    
+    if for_attribute.nil?
+      expect(for_attribute).to_not be_empty,
+        "Expected label's for attribute to be set to a non empty value, was '#{for_attribute}' instead."
+    else
+      all_inputs = all("input")
+  
+      all_inputs.each do |input|
+        if input[:id] == for_attribute
+          if type == "email"
+            input.set first_user.email
+          else
+            input.set first_user.username
+          end
+        end
+      end
+    end
+
+    within(:css, "form") do
+      # fill_in "Email", with: first_user.email
+      fill_in "Password", with: first_user.password
+      find("button", :text => /Sign in/i ).click
+    end
+
+
+    visit "/photos"
+    within(:css, "form") do
+      fill_in "Image", with: "https://some.test/image-#{Time.now.to_i}.jpg"
+      fill_in "Caption", with: "Eat some pizza"
+      find("button", :text => /Add photo/i ).click
+    end
+    
+    expect(page).to have_text("Eat some pizza"),
+      "Expected page to contain #{"Eat some pizza"}, but didn't."
+  end
+end
+
+describe "/photos/[ID] - Update photo form" do
+  it "does not display Update photo form when photo does not belong to current user", points: 2 do
+    first_user = User.new
+    first_user.password = "password"
+    first_user.username = "alice"
+    if User.has_attribute?(:email)
+      first_user.email = "alice@example.com"
+    end
+    first_user.save
+
+    bob = User.new
+    bob.password = "password"
+    bob.username = "bob"
+    if User.has_attribute?(:email)
+      bob.email = "bob@example.com"
+    end
+    bob.save
+
+    photo = Photo.new
+    photo.image = "https://some.test/image-#{Time.now.to_i}.jpg"
+    photo.caption = "Some test caption #{Time.now.to_i}"
+    photo.owner_id = bob.id
+    photo.save
+
+    visit "/user_sign_in"
+        
+    type = "email"
+    name_label = all("label", :text => /Email/i)
+    if name_label.count.positive?
+      name_label = name_label.first
+    else
+      name_label = find("label", :text => /Username/i)
+      type = "username"
+    end
+    
+    for_attribute = name_label[:for]
+    
+    if for_attribute.nil?
+      expect(for_attribute).to_not be_empty,
+        "Expected label's for attribute to be set to a non empty value, was '#{for_attribute}' instead."
+    else
+      all_inputs = all("input")
+  
+      all_inputs.each do |input|
+        if input[:id] == for_attribute
+          if type == "email"
+            input.set first_user.email
+          else
+            input.set first_user.username
+          end
+        end
+      end
+    end
+
+    within(:css, "form") do
+      # fill_in "Email", with: first_user.email
+      fill_in "Password", with: first_user.password
+      find("button", :text => /Sign in/i ).click
+    end
+
+    
+    visit "/photos/#{photo.id}"
+
+
+    # expect(page).to_not have_text("Update photo")
+    expect(page).not_to have_tag("button", :text => /Update photo/i),
+      "Expected page to not have a button with the text 'Update photo', but found one."
   end
 end
 
 describe "/photos/[ID] - Update photo form" do
   it "displays Update photo form when photo belongs to current user", points: 2 do
     first_user = User.new
-    first_user.email = "first@example.com"
-    if User.has_attribute?(:username)
-      first_user.username = "alice"
-    end
     first_user.password = "password"
+    first_user.username = "alice"
+    if User.has_attribute?(:email)
+      first_user.email = "alice@example.com"
+    end
     first_user.save
 
     photo = Photo.new
@@ -82,7 +279,7 @@ describe "/photos/[ID] - Update photo form" do
     photo.save
 
     visit "/user_sign_in"
-    
+        
     type = "email"
     name_label = all("label", :text => /Email/i)
     if name_label.count.positive?
@@ -114,24 +311,27 @@ describe "/photos/[ID] - Update photo form" do
     within(:css, "form") do
       # fill_in "Email", with: first_user.email
       fill_in "Password", with: first_user.password
-      click_on "Sign in"
+      find("button", :text => /Sign in/i ).click
     end
-    
+
+
     visit "/photos/#{photo.id}"
 
 
-    expect(page).to have_text("Update photo")
+    # expect(page).to have_text("Update photo")
+    expect(page).to have_tag("button", :text => /Update photo/i),
+      "Expected page to have a button with the text 'Update photo', but didn't find one."
   end
 end
 
 describe "/photos/[ID] - Delete this photo button" do
   it "displays Delete this photo button when photo belongs to current user", points: 2 do
     first_user = User.new
-    first_user.email = "first@example.com"
-    if User.has_attribute?(:username)
-      first_user.username = "alice"
-    end
     first_user.password = "password"
+    first_user.username = "alice"
+    if User.has_attribute?(:email)
+      first_user.email = "alice@example.com"
+    end
     first_user.save
 
     photo = Photo.new
@@ -141,7 +341,7 @@ describe "/photos/[ID] - Delete this photo button" do
     photo.save
 
     visit "/user_sign_in"
-    
+        
     type = "email"
     name_label = all("label", :text => /Email/i)
     if name_label.count.positive?
@@ -169,29 +369,34 @@ describe "/photos/[ID] - Delete this photo button" do
         end
       end
     end
-      
+
     within(:css, "form") do
       # fill_in "Email", with: first_user.email
       fill_in "Password", with: first_user.password
-      click_on "Sign in"
+      find("button", :text => /Sign in/i ).click
     end
+
     
+
     visit "/photos/#{photo.id}"
 
-    expect(page).to have_content(first_user.username)
+    expect(page).to have_content(first_user.username),
+      "Expected to find the User's username, but didn't find it."
 
-    expect(page).to have_link("Delete this photo")
+    # expect(page).to have_link("Delete this photo")
+    expect(page).to have_tag("a", :text => /Delete this photo/i ),
+      "Expected page to have a link with the text 'Delete this photo', but didn't find one."
   end
 end
 
 describe "/photos/[ID] — Add comment form" do
   it "automatically associates comment with signed in user and current photo", points: 2 do
     first_user = User.new
-    first_user.email = "first@example.com"
-    if User.has_attribute?(:username)
-      first_user.username = "alice"
-    end
     first_user.password = "password"
+    first_user.username = "alice"
+    if User.has_attribute?(:email)
+      first_user.email = "alice@example.com"
+    end
     first_user.save
 
     photo = Photo.new
@@ -201,7 +406,7 @@ describe "/photos/[ID] — Add comment form" do
     photo.save
 
     visit "/user_sign_in"
-    
+        
     type = "email"
     name_label = all("label", :text => /Email/i)
     if name_label.count.positive?
@@ -233,8 +438,9 @@ describe "/photos/[ID] — Add comment form" do
     within(:css, "form") do
       # fill_in "Email", with: first_user.email
       fill_in "Password", with: first_user.password
-      click_on "Sign in"
+      find("button", :text => /Sign in/i ).click
     end
+
 
     test_comment = "Hey, what a nice app you're building!"
 
@@ -242,10 +448,214 @@ describe "/photos/[ID] — Add comment form" do
 
     fill_in "Comment", with: test_comment
 
-    click_on "Add comment"
+    find("button", :text => /Add comment/i ).click
 
     added_comment = Comment.where({ :author_id => first_user.id, :photo_id => photo.id, :body => test_comment }).at(0)
 
-    expect(added_comment).to_not be_nil
+    expect(added_comment).to_not be_nil,
+      "Expected to create a new Comment record by visiting the photo details page, entering text in the 'Comment' field and clicking 'Add comment' but comment did not save."
+  end
+end
+
+describe "/photos/[ID] - Like Form" do
+  it "automatically populates photo_id and fan_id with current photo and signed in user", points: 1 do
+    first_user = User.new
+    first_user.password = "password"
+    first_user.username = "alice"
+    if User.has_attribute?(:email)
+      first_user.email = "alice@example.com"
+    end
+    first_user.save
+
+    photo = Photo.new
+    photo.image = "https://some.test/image-#{Time.now.to_i}.jpg"
+    photo.caption = "Some test caption #{Time.now.to_i}"
+    photo.owner_id = first_user.id
+    photo.likes_count = 0
+    photo.save
+
+    visit "/user_sign_in"
+        
+    type = "email"
+    name_label = all("label", :text => /Email/i)
+    if name_label.count.positive?
+      name_label = name_label.first
+    else
+      name_label = find("label", :text => /Username/i)
+      type = "username"
+    end
+    
+    for_attribute = name_label[:for]
+    
+    if for_attribute.nil?
+      expect(for_attribute).to_not be_empty,
+        "Expected label's for attribute to be set to a non empty value, was '#{for_attribute}' instead."
+    else
+      all_inputs = all("input")
+  
+      all_inputs.each do |input|
+        if input[:id] == for_attribute
+          if type == "email"
+            input.set first_user.email
+          else
+            input.set first_user.username
+          end
+        end
+      end
+    end
+
+    within(:css, "form") do
+      # fill_in "Email", with: first_user.email
+      fill_in "Password", with: first_user.password
+      find("button", :text => /Sign in/i ).click
+    end
+
+
+    old_likes_count = Like.where({ :photo_id => photo.id }).count
+
+    visit "/photos/#{photo.id}"
+    
+    find("button", :text => /Like/i ).click
+
+    expect(photo.likes.count).to be >= (old_likes_count + 1),
+      "Expected clicking the 'Like' button to add a record to the Likes table, but it didn't."
+  end
+end
+
+describe "/photos/[ID] - Delete Like link" do
+  it "displays 'Delete Like' link if current user has already liked the Photo", points: 1 do
+    first_user = User.new
+    first_user.password = "password"
+    first_user.username = "alice"
+    if User.has_attribute?(:email)
+      first_user.email = "alice@example.com"
+    end
+    first_user.save
+
+    photo = Photo.new
+    photo.image = "https://some.test/image-#{Time.now.to_i}.jpg"
+    photo.caption = "Some test caption #{Time.now.to_i}"
+    photo.owner_id = first_user.id
+    photo.likes_count = 0
+    photo.save
+
+    like = Like.new
+    like.photo_id = photo.id
+    like.fan_id = first_user.id
+    like.save
+
+    visit "/user_sign_in"
+        
+    type = "email"
+    name_label = all("label", :text => /Email/i)
+    if name_label.count.positive?
+      name_label = name_label.first
+    else
+      name_label = find("label", :text => /Username/i)
+      type = "username"
+    end
+    
+    for_attribute = name_label[:for]
+    
+    if for_attribute.nil?
+      expect(for_attribute).to_not be_empty,
+        "Expected label's for attribute to be set to a non empty value, was '#{for_attribute}' instead."
+    else
+      all_inputs = all("input")
+  
+      all_inputs.each do |input|
+        if input[:id] == for_attribute
+          if type == "email"
+            input.set first_user.email
+          else
+            input.set first_user.username
+          end
+        end
+      end
+    end
+
+    within(:css, "form") do
+      # fill_in "Email", with: first_user.email
+      fill_in "Password", with: first_user.password
+      find("button", :text => /Sign in/i ).click
+    end
+
+
+    visit "/photos/#{photo.id}"
+
+    expect(page).to have_tag("a", :text => /Delete Like/i),
+      "Expected page to have a link with the text 'Delete Like', but didn't find one."
+  end
+end
+
+describe "/photos/[ID] - Delete Like link" do
+  it "removes the Like record between the current user and current photo when clicked", points: 1 do
+    first_user = User.new
+    first_user.password = "password"
+    first_user.username = "alice"
+    if User.has_attribute?(:email)
+      first_user.email = "alice@example.com"
+    end
+    first_user.save
+
+    photo = Photo.new
+    photo.image = "https://some.test/image-#{Time.now.to_i}.jpg"
+    photo.caption = "Some test caption #{Time.now.to_i}"
+    photo.owner_id = first_user.id
+    photo.likes_count = 0
+    photo.save
+
+    like = Like.new
+    like.photo_id = photo.id
+    like.fan_id = first_user.id
+    like.save
+
+    visit "/user_sign_in"
+        
+    type = "email"
+    name_label = all("label", :text => /Email/i)
+    if name_label.count.positive?
+      name_label = name_label.first
+    else
+      name_label = find("label", :text => /Username/i)
+      type = "username"
+    end
+    
+    for_attribute = name_label[:for]
+    
+    if for_attribute.nil?
+      expect(for_attribute).to_not be_empty,
+        "Expected label's for attribute to be set to a non empty value, was '#{for_attribute}' instead."
+    else
+      all_inputs = all("input")
+  
+      all_inputs.each do |input|
+        if input[:id] == for_attribute
+          if type == "email"
+            input.set first_user.email
+          else
+            input.set first_user.username
+          end
+        end
+      end
+    end
+
+    within(:css, "form") do
+      # fill_in "Email", with: first_user.email
+      fill_in "Password", with: first_user.password
+      find("button", :text => /Sign in/i ).click
+    end
+
+
+    old_likes_count = Like.where({ :photo_id => photo.id }).count
+    
+    visit "/photos/#{photo.id}"
+    
+    find("a", :text => /Delete Like/i ).click
+    
+    new_likes_count = Like.where({ :photo_id => photo.id }).count
+
+    expect(new_likes_count).to be >= (old_likes_count - 1),
+      "Expected clicking the 'Delete Like' link to remove a record to the Likes table, but it didn't."
   end
 end
